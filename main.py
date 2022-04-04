092a3b063fdd90b804981e0271d0b1c5627dc7cb
dhruv50ae <linkdp2012@gmail.com>
2022-04-04 15:19:58 +0100

    Used all 4 string ideas

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Hello" + " " + "Dhruv")
+print("Day 1 - String Manipulation")
+print('String concatenation is done with the "+" sign')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
